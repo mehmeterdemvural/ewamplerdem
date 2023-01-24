f5a75b436460e5752f92ef8da4dafa88c51b9f6f
Mehmet ErdemVURAL <mehmeterdemvural@gmail.com>
2023-01-24 18:19:47 +0300

    first commit

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,15 @@ import { ExampleComponent } from 'ewamplerdem'
 import 'ewamplerdem/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div>
+      <ExampleComponent type='primary' />
+      <ExampleComponent type='default' />
+      <ExampleComponent type='dashed' />
+      <ExampleComponent type='text' />
+      <ExampleComponent type='link' />
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = ({ type }) => {
+  return (
+    <div className={styles.test}>
+      <button type={type}> Button Type : {type}</button>
+    </div>
+  )
 }
